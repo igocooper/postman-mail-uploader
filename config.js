f3bcdb7ed74abb1fcc81d0fe77dc44761d1d0f38
Dmytro Lytvyn <kilmer@zeoalliance.com>
2018-04-19 15:46:53 +0300

    last changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config  = {
     template: '1669',
     locales: ['en','cs','da','de','es','fi','fr','it','ja','ko','nl','no','pl','pt','ru','sv','tr','zh'],
     credentials: {
-        login: 'Test@zeoalliance.com',
+        login: 'Test@gmail.com',
         password: 'YourPassword'
         
     }
