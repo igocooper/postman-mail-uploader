876dd8d5247431ba4449916c3d8e4c3d1271b8c4
Dmytro Lytvyn <kilmer@zeoalliance.com>
2018-04-19 14:12:19 +0300

    added ReadMe, package.json

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config  = {
     template: '1669',
-    locales: ['en','cs','da','de'],
+    locales: ['en','cs','da','de','es','fi','fr','it','ja','ko','nl','no','pl','pt','ru','sv','tr','zh'],
     credentials: {
-        login: '',
-        password: ''
+        login: 'Test@zeoalliance.com',
+        password: 'YourPassword'
         
     }
 };
