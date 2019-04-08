ac1ecd9ac1f2a4032874c4ea8e2ce8b88bdbacc7
Dima Lytvyn <killmer@MacBook-Pro-Killmer.local>
2019-04-08 15:57:49 +0300

    deleted sensivity data

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config  = {
     template: '1708',
     locales: ['en','cs','da','de','es','fi','fr','it','ja','ko','nl','no','pl','pt','ru','sv','tr','zh'],
     credentials: {
-        login: 'kilmer@zeoalliance.com',
-        password: 'Gfhjkm743345'
+        login: 'test@mail.com',
+        password: 'testpassword'
         
     }
 };
